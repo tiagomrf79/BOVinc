14d642ac9acbf3f2ed0b69a4e0df5e9fc0f99364
Tiago <tiagomrf@hotmail.com>
2023-10-14 14:32:35 +0000

    trying different docker configurations to work with azure docker compose preview 1

--- FarmsAPI/Controllers/SeedController.cs
+++ FarmsAPI/Controllers/SeedController.cs
@@ -28,12 +28,10 @@ public class SeedController : ControllerBase
     }
 
     [HttpPut]
+    [ApiExplorerSettings(IgnoreApi = true)]
     [ResponseCache(NoStore = true)]
-    public async Task<IActionResult> FarmsData()
+    public async Task<IActionResult> SeedFarmsData()
     {
-        using var transaction =_context.Database.BeginTransaction();
-        await _context.Farms.ExecuteDeleteAsync();
-
         CsvConfiguration csvConfiguration = new CsvConfiguration(CultureInfo.GetCultureInfo("pt-PT"))
         {
             HasHeaderRecord = true,
@@ -41,35 +39,58 @@ public class SeedController : ControllerBase
         };
 
         string fileFullPath = Path.Combine(_env.ContentRootPath, "Data/farms_dummy_dataset.csv");
-        using StreamReader reader = new StreamReader(fileFullPath);
-        using CsvReader csvReader = new CsvReader(reader, csvConfiguration);
 
-        DateTime now = DateTime.Now;
+        int addedRows = 0;
         int skippedRows = 0;
 
-        IEnumerable<FarmRecord> records = csvReader.GetRecords<FarmRecord>();
+        DateTime now = DateTime.Now;
+        IEnumerable<FarmRecord> recordsFromCsv = new List<FarmRecord>();
 
-        foreach (FarmRecord record in records)
+        using (StreamReader reader = new StreamReader(fileFullPath))
         {
-            if (!record.Id.HasValue || string.IsNullOrEmpty(record.Name))
+            using (CsvReader csvReader = new CsvReader(reader, csvConfiguration))
+            {
+                recordsFromCsv = csvReader.GetRecords<FarmRecord>().ToList();
+            }
+        }
+
+        Dictionary<int, Farm> recordsFromDb = await _context.Farms.ToDictionaryAsync(f => f.Id);
+
+        foreach (FarmRecord csvRecord in recordsFromCsv)
+        {
+            // value from CSV is invalid
+            if (!csvRecord.Id.HasValue || string.IsNullOrEmpty(csvRecord.Name))
             {
                 skippedRows++;
                 continue;
             }
 
-            Farm recordToAdd = _mapper.Map<Farm>(record);
+            // value from CSV already exists
+            if (recordsFromDb.ContainsKey(csvRecord.Id.Value))
+            {
+                skippedRows++;
+                continue;
+            }
+
+            Farm recordToAdd = _mapper.Map<Farm>(csvRecord);
             recordToAdd.DateCreated = now;
             recordToAdd.DateUpdated = now;
 
             _context.Farms.Add(recordToAdd);
+            addedRows++;
         }
 
+        using var transaction =_context.Database.BeginTransaction();
+
+        _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Farms ON");
         await _context.SaveChangesAsync();
+        _context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Farms OFF");
+
         transaction.Commit();
 
         return new JsonResult(new
         {
-            Farms = _context.Farms.Count(),
+            AddedRows = addedRows,
             SkippedRows = skippedRows
         });
     }
