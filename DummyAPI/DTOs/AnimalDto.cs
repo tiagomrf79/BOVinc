74503b05b31642a4858d37775e26826a1ee32f89
Tiago <tiagomrf@hotmail.com>
2024-01-03 22:50:30 -0100

    complete AnimalDto, still missing dummy data

--- DummyAPI/DTOs/AnimalDto.cs
+++ DummyAPI/DTOs/AnimalDto.cs
@@ -3,8 +3,8 @@
 public class AnimalDto
 {
     public int Id { get; set; }
-    public string RegistrationId { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
+    public string? RegistrationId { get; set; }
+    public string? Name { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public int GenderId { get; set; }
     public string Gender { get; set; } = string.Empty;
@@ -12,4 +12,16 @@ public class AnimalDto
     public string Breed { get; set; } = string.Empty;
     public int CategoryId { get; set; }
     public string Category { get; set; } = string.Empty;
+    public int? DamId { get; set; }
+    public string? DamName { get; set; }
+    public int? SireId { get; set; }
+    public string? SireName { get; set; }
+    public int? LactationNumber { get; set; }
+    public DateOnly? LastCalvingDate { get; set; }
+    public int? BreedingStatusId { get; set; }
+    public string? BreedingStatus { get; set; }
+    public DateOnly? LastBreedingDate { get; set; }
+    public int? BreedingBullId { get; set; }
+    public DateOnly? LastDryDate { get; set; }
+    public DateOnly? DueDateForCalving { get; set; }
 }
