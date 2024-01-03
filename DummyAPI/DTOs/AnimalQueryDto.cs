59642fef6dd5f3e64bd245d3224a08e25e03be06
Tiago <tiagomrf@hotmail.com>
2024-01-03 22:05:52 -0100

    changed swagger annotations

--- DummyAPI/DTOs/AnimalQueryDto.cs
+++ DummyAPI/DTOs/AnimalQueryDto.cs
@@ -11,32 +11,38 @@ public class AnimalQueryDto<T> : IValidatableObject
 
 
     /// <summary>Filter for breed</summary>
+    [Required]
     [DefaultValue(0)]
     public int BreedFilter { get; set; } = 0;
 
     /// <summary>Filter for gender</summary>
+    [Required]
     [DefaultValue(0)]
     public int GenderFilter { get; set; } = 0;
 
 
     /// <summary>Initial index to start pagination</summary>
+    [Required]
     [Range(0, int.MaxValue)]
     [DefaultValue(0)]
     public int StartIndex { get; set; } = 0;
 
     /// <summary>Number of records per page</summary>
+    [Required]
     [Range(1, 100)]
     [DefaultValue(10)]
     public int MaxRecords { get; set; } = 10;
 
 
     /// <summary>Sort attribute</summary>
+    [Required]
     [DefaultValue("Name")]
-    public string? SortAttribute { get; set; } = "Name";
+    public string SortAttribute { get; set; } = "Name";
 
     /// <summary>Sort order</summary>
+    [Required]
     [DefaultValue("ASC")]
-    public string? SortDirection { get; set; } = "ASC";
+    public string SortDirection { get; set; } = "ASC";
 
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
