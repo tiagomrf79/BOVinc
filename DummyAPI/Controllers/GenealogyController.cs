7bf08eb1382e1ecc5a00ac8249054e3e0f91c63a
Tiago <tiagomrf@hotmail.com>
2024-01-08 21:51:48 -0100

    ascendants method and DTO

--- DummyAPI/Controllers/GenealogyController.cs
+++ DummyAPI/Controllers/GenealogyController.cs
@@ -48,7 +48,53 @@ public class GenealogyController : ControllerBase
             },
         };
 
-        return listToReturn;
+        return Ok(listToReturn);
     }
 
+
+    [HttpGet("Ascendants", Name = "GetAscendants")]
+    [SwaggerOperation(Summary = "Retrieves the ascendants of a given animal")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Returns some basic information about the ascendants", typeof(IEnumerable<AscendantDto>))]
+    public async Task<ActionResult<IEnumerable<AscendantDto>>> GetAscendants(
+        [FromQuery, SwaggerParameter("Animal ID", Required = true)] int animalId)
+    {
+        string name = "Marquesa";
+
+        AscendantDto dtoToReturn = new()
+        {
+            Id = animalId,
+            AscendantLabel = name,
+            FeatureScore = "12.459 lt",
+            Dam = new AscendantDto()
+            {
+                Id = 87,
+                AscendantLabel = "Malhada",
+                FeatureScore = "",
+                Dam = new AscendantDto()
+                {
+                    Id = 66,
+                    AscendantLabel = "PT 555 111 222",
+                    FeatureScore = "9.864 lt"
+                },
+                Sire = new AscendantDto()
+                {
+                    Id = 156,
+                    AscendantLabel = "PT 654 123321"
+                }
+            },
+            Sire = new AscendantDto()
+            {
+                Id = 156,
+                AscendantLabel = "Tsubasa",
+                FeatureScore = "+1600 TPI",
+                Sire = new AscendantDto()
+                {
+                    Id = 88,
+                    AscendantLabel = "Rambo"
+                }
+            }
+        };
+
+        return Ok(dtoToReturn);
+    }
 }
---
+++ DummyAPI/DTOs/AscendantDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DummyAPI.DTOs;
+
+public class AscendantDto
+{
+    public int Id { get; set; }
+    public string AscendantLabel { get; set; } = string.Empty;
+    public string? FeatureScore { get; set; }
+    public AscendantDto? Dam {  get; set; }
+    public AscendantDto? Sire {  get; set; }
+}
+
